8cf159b1607a58ff734c203a352f10a24b934180
Cowan Hahn <chahn100i@gmail.com>
2024-05-07 21:30:59 -0400

    Random Templates

--- obj/Debug/net8.0/Pancake-Rotation-Tracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pancake-Rotation-Tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pancake-Rotation-Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77a6402e70603ee268efe7cb97fcb5c2b8b1ce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9452d8880cb8bc9adeac2ab1dc9c81543a64f977")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pancake-Rotation-Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pancake-Rotation-Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
